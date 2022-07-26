737625c4536570d177bf7a421dc9f06ec9b5b4a6
Brian Alexander <balexand@gmail.com>
2022-07-26 13:39:11 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Gql.MixProject do
     [
       app: :gql,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
